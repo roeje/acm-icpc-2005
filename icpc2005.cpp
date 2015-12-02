8666d4a7e8bf5c58fe6b6b3b62bb4a7cf190d851
Jesse Roe <jessedroe@gmail.com>
2015-12-03 01:13:54 +0800

    Removed unnecessary include tags . added vecotor to hold map data

--- icpc2005.cpp
+++ icpc2005.cpp
@@ -21,18 +21,25 @@ string output = "";
 string readData(istream& data) {
     string header;
     string mainData;
-    getline(data,header);
-    string c;
-    while (getline(data, c) &&  c != "" && c != "") {
-        mainData += c;
-//        if(getline(data, c) && c != ""){
-//            mainData += c;
-//            break;
-//        }
+    string tempData;
+    int x = 0;
+
+    while (getline(data, tempData) && tempData != "") {
+        if(x == 0){
+            header = tempData;
+            x++;
+        }
+        else {
+            mainData += tempData;
+//            cout << mainData << endl;
+        }
     }
+
+//    cout << mainData << endl;
     headerKey(header);
     int groupSize = returnBinary(mainData.substr(0,3));
     mainData = mainData.substr(3);
+//    cout << "starting Recursion" << endl;
     return returnMessage(mainData, groupSize);
 }
 
@@ -53,49 +60,12 @@ int returnBinary(string pString){
 
 void headerKey(string& pString){
     //keeps track of position in string
-    int position = 0;
+    int count = 0;
+    vector <string> binaryToChar = {"0", "00", "01", "10", "000", "001", "010", "011", "100", "101", "110"};
     //iterates through the string
-    for(std::string::iterator it = pString.begin(); it != pString.end(); ++it) {
-        if(position == 0){
-            tempMap["0"] = *it;
-        }
-        else
-        if(position == 1){
-            tempMap["00"] = *it;
-        }
-        else
-        if(position == 2){
-            tempMap["01"] = *it;
-        }
-        else
-        if(position == 3){
-            tempMap["10"] = *it;
-        }
-        else
-        if(position == 4){
-            tempMap["000"] = *it;
-        }
-        else
-        if(position == 5){
-            tempMap["001"] = *it;
-        }
-        else
-        if(position == 6){
-            tempMap["011"] = *it;
-        }
-        else
-        if(position == 7){
-            tempMap["100"] = *it;
-        }
-        else
-        if(position == 8){
-            tempMap["101"] = *it;
-        }
-        else
-        if(position == 9){
-            tempMap["110"] = *it;
-        }
-        position++;
+    for(std::string::iterator itr = pString.begin(); itr != pString.end(); ++itr) {
+        tempMap[binaryToChar.at(count)] = *itr;
+        count++;
     }
 }
 
@@ -129,22 +99,26 @@ string returnMessage(string& pString, int groupSize) {
 }
 
 int main() {
-//    string a = "$#**/";
-//    string b = "0100000101101100011100101000";
-//
-//    headerKey(a);
-//    cout << "output tempMat at 0" << endl;
-//    cout << tempMap.at("0") << endl;
-//    int groupSize = returnBinary(b.substr(0,3));
-//    cout << groupSize << endl;
-//    b = b.substr(3);
-//    cout << b << endl;
-//    cout << returnMessage(b, groupSize) << endl;
-
-
+#ifdef ONLINE_JUDGE
+    string testData = "$#**/\n"
+            "01\n"
+            "00000\n"
+            "1011011\n"
+            "000111\n"
+            "00101000\n";
+    istringstream data (testData);
+#else
     istream& data = cin;
-    cout << readData(data);
+#endif
+    cout << readData(data) << "\n" << endl;
 }
+
+//$#**/\r
+//01\r
+//00000\r
+//1011011\r
+//000111\r
+//00101000\r
 
 //$#**/
 //01
@@ -152,3 +126,19 @@ int main() {
 //1011011
 //000111
 //00101000
+
+//$#**/
+//01
+//00000
+//1011011
+//000111
+//$#**/
+//01
+//00000
+//1011011
+//000111
+
+//**&^$
+//000101010
+//001010111010
+//01010101011
